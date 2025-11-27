b2a3efd643aa9ff953251bc297a806db5158b823
ibrahim3761 <mdibrahimabdullah1@gmail.com>
2025-11-27 23:00:10 +0600

    created-dashboard-layout

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,14 @@
+import DashboardSidebar from "@/app/dashboard/components/DashboardSidebar";
+import DashboardTopBar from "@/app/dashboard/components/DashboardTopBar";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div style={{ backgroundColor: "#EEEEEE", color: "#1A232D" }} className="min-h-screen">
+      <DashboardTopBar /> 
+      <DashboardSidebar />
+      <main className="md:ml-[250px] pt-[60px] pb-[70px] p-6">
+        {children}
+      </main>
+    </div>
+  );
+}
